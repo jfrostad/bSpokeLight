069f69965581d2f45a6dbf0572cbee0dade010ba
jfrostad <jfrostad@uw.edu>
2018-08-17 17:15:48 -0700

    adding new images and a new firmware

--- create.sh
+++ create.sh
@@ -6,7 +6,9 @@ sudo docker run --rm -it -v $PWD:/home bspoke bSpokeLight \
 	 --offset 6 \
 	 --rotation 8 \
      --output fware.bin \
-     imgs/shodan.gif 20 imgs/goatse.png 10 #imgs/flameboy.jpg 10
+     imgs/west_world_goofy.jpg 20 imgs/molly.jpg 10 imgs/johnny_5_goofy.jpg 20 \
+     imgs/walle_eve_pix.png 20 imgs/cyclops_robot.jpg 20 imgs/ex_machina_colorful.jpg 10 \
+     imgs/terminator_small.jpg 20 imgs/term_pixel.png 20
 
  #flash it to wheel
  sudo stcgal -P stc12 fware.bin
